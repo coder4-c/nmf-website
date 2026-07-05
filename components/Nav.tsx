54684773194bbfe38306ad596abdd84de4849707
Marion Kipruto <marionrutto21@gmail.com>
2026-07-05 20:57:44 +0300

    increased size to 26

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -22,7 +22,7 @@ export default function Nav() {
             alt="Next Mission Fitness"
             width={160}
             height={107}
-            className="h-20 w-auto"
+            className="h-26 w-auto"
             priority
           />
         </Link>
